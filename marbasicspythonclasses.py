e3d5b9a8e8c12af4f0dbeccaa96baf938e6aed51
Raymond Mar <raym61@hotmail.com>
2020-03-05 18:13:44 -0800

    Added more classes from YouTube William Fiset

--- marbasicspythonclasses.py
+++ marbasicspythonclasses.py
@@ -1,5 +1,6 @@
 #Basics Python Classes Combine All Lessons Learned One Stop Book One Stop Documentation
 #thenewbostonallvideos
+#williamfisetallvideos
 
 class Thenewbostonenemy:
 	life = 3
@@ -112,17 +113,90 @@ kathy.move() #return Kathy is moving Mario
 #raymond.fireflower() #error message AttributeError: 'Mario' object has no attribute 'fireflower'
 edward.eatshroom() #return Edward made Mario eat the shroom
 edward.move() #return Edward is moving Mario
-from operator import attrgetter
-class User3:
-	def __init__(self, name, anumber):
+
+class Boat():
+	def __init__(self):
+		self.definecargoweight = 23
+	def printdefinecargoweight(self):
+		return self.definecargoweight
+defineobjectBoat1 = Boat()
+print("print definecargoweight value",defineobjectBoat1.definecargoweight) #print print definecargoweight value 23
+print(defineobjectBoat1) #print <__main__.Boat object at 0x7f4b68c0a128>
+print(defineobjectBoat1.printdefinecargoweight()) #print 23
+defineobjectBoat2 = Boat()
+print(defineobjectBoat2) #print <__main__.Boat object at 0x7fef924adac8>
+print(defineobjectBoat2.printdefinecargoweight()) #print 23
+class Character():
+	classvariable = 100
+	def __init__(self, name):
 		self.name = name
-		self.user_id = anumber
-	def __repr__(self):
-		return self.name+" : "+str(self.user_id)
-users3list = [User3("Bucky", 43),User3("Sally", 5),User3("Tuna", 61),User3("Brian", 2),	User3("Joby", 77),User3("Amanda", 9)]
-print(users3list) #print [Bucky : 43, Sally : 5, Tuna : 61, Brian : 2, Joby : 77, Amanda : 9]
-print(sorted(users3list,key=attrgetter("name"))) #print [Amanda : 9, Brian : 2, Bucky : 43, Joby : 77, Sally : 5, Tuna : 61]
-print(sorted(users3list,key=attrgetter("user_id"))) #print [Brian : 2, Sally : 5, Amanda : 9, Bucky : 43, Tuna : 61, Joby : 77]
-
-
+		self.health = Character.classvariable
+	def finalhealth(self):
+		namelength = len(self.name)
+		return self.health - namelength
+defineobjectraymond = Character("raymond")
+print(defineobjectraymond.finalhealth()) #print 93
+class AlsoCharacter():
+	classvariable = 100
+	def __init__(self, name):
+		self.name = name
+	def finalhealth(self):
+		namelength = len(self.name)
+		return AlsoCharacter.classvariable - namelength
+defineobjectraymond = AlsoCharacter("raymond")
+print(defineobjectraymond.finalhealth()) #print 93
+class Human():
+	def __init__(self, initializevariable1name, initializevariable2gender):
+		self.initializevariable1name = initializevariable1name
+		self.initializevariable2gender = initializevariable2gender
+	def printname(self):
+		return self.initializevariable1name
+	def printgender(self):
+		return self.initializevariable2gender
+	def speaksomething(self, textvariable):
+		print("{} said {}".format(self.initializevariable1name, textvariable))
+	def speaktwosometheings(self, textvariableone, textvariabletwo):
+		print("%s said the following:" %(self.initializevariable1name))
+		print(textvariableone+" and "+textvariabletwo)
+	def givemethreenouns(self, *multiplenouns):
+		print(multiplenouns)
+		print(list(multiplenouns))
+		print("\n".join(multiplenouns))
+raymond = Human("nameraymond","gendermale")
+print(raymond) #print <__main__.Human object at 0x7ff84eb23278>
+print("print init variables "+raymond.initializevariable1name) #print print init variables nameraymond
+print("print init variables "+raymond.initializevariable2gender) #print print init variables gendermale
+print(raymond.printname()) #print nameraymond
+print(raymond.printgender()) #print gendermale
+raymond.speaksomething("sentence for the textvariable for speaksomething()") #print nameraymond said sentence for the textvariable for speaksomething()
+raymond.speaktwosometheings("the first input","the second input") #print nameraymond said the following\n the first input and the second input
+raymond.givemethreenouns("cat","dog","bird") #print ('cat', 'dog', 'bird')\n ['cat', 'dog', 'bird']\n cat\n dog\n bird
+class FunctionOutsideClass():
+	def choosemathoperation(self, tellmemathoperationfunction, *numbers):
+		print("{}".format(tellmemathoperationfunction(*numbers)))
+def mathoperationsadd(a, b):
+	return a + b
+def mathoperationsmultiply(a, b):
+	return a * b
+defineobjectadd1 = FunctionOutsideClass()
+defineobjectadd1.choosemathoperation(mathoperationsadd, 3, 5) #print 8
+defineobjectmultiply1 = FunctionOutsideClass()
+defineobjectmultiply1.choosemathoperation(mathoperationsmultiply, 4, 8) #print 32
+defineobjectadd2 = FunctionOutsideClass()
+defineobjectadd2.choosemathoperation(mathoperationsadd, 100, 333) #print 433
+class ClassName():
+	def __init__(self):
+		self._privatevariable = "private"
+		self.publicvariable = "public"
+	def _privateMethod(self):
+		pass
+	def _privateMethod2(self):
+		pass
+	def publicMethod(self):
+		return 5
+c = ClassName()
+print(c._privateMethod()) #print None
+print(c.publicMethod()) #print 5
+print(c._privatevariable) #print private
+print(c.publicvariable) #print public
 
