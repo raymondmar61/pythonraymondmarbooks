5bcc9847c6adfd12c5496526874fbd9a7ab49afe
raymondmar61 <raym61@hotmail.com>
2023-10-20 13:39:29 -0700

    If statements and for loops basics

--- ultimateifelseforwhile.py
+++ ultimateifelseforwhile.py
@@ -19,12 +19,88 @@ else:
     print("Third if statement independent greater than fifty applies at 50 with an else less than or equal to fifty applies.") #print Third if statement independent greater than fifty applies at 50 with an else less than or equal to fifty applies.
 if 10 <= apples <= 20:
     print("If statement between if statement greater than equal less than equal") #print If statement between if statement greater than equal less than equal
+grade = 77
+if grade >= 90:
+    print("A")
+elif grade >= 80:
+    print("B")
+elif grade >= 70:
+    print("C") #print C
+elif grade >= 60:
+    print("D")
+else:
+    print("If statement exits at the first True")
 truevariable = True
 if truevariable:
-    print("The truevariable is True.  Print the sentence.")
+    print("The truevariable is True.  Print the sentence.") #print The truevariable is True.  Print the sentence.
 truesentence = "Anything between the quotes"
 if truesentence:
-    print("The truesentence is True.  Print the sentence.")
+    print("The truesentence is True.  Print the sentence.") #print The truevariable is True.  Print the sentence.
+andortrue = True
+andorfalse = False
+if andortrue and andorfalse:
+    print("if and Execute when both conditions are true.  True and True is True.")
+else:
+    print("if and Execute when the conditions are false.  True and False or False and True is False.") #print if and Execute when the conditions are false.  True and False or False and True is False.
+if andortrue or andorfalse:
+    print("if or Execute when either condition is true.  True or False, or False or True, is True.") #print if or Execute when either condition is true.  True or False, or False or True, is True.
+else:
+    print("if or Execute when either condition is false.  False or False is False.")
+if not(andortrue) or andorfalse:
+    print("if or Execute when either condition is true.  True or False, or False or True, is True.")
+else:
+    print("if or Execute when either condition is false.  False or False is False.") #print if or Execute when either condition is false.  False or False is False.
+numberintuple = 7
+if numberintuple in (7, 11):
+    print("Winner") #print Winner
+elif numberintuple in (2, 3, 12):
+    print("Loser")
+else:
+    print("Point number established")
+name = "Mary"
+password = "swordfish"
+if name == "Mary":
+    if password == "swordfish":
+        print("Name and password correct") #print Name and password correct
+    else:
+        print("Name or password incorrect")
+else:
+    print("Name incorrect")
+name = "Barry"
+password = "swordfish"
+if name == "Mary":
+    if password == "swordfish":
+        print("Name and password correct")
+    else:
+        print("Name or password incorrect")
+else:
+    print("Name incorrect") #print Name incorrect
+name = "Mary"
+password = "nested ifs nested if statements"
+if name == "Mary":
+    if password == "swordfish":
+        print("Name and password correct")
+    else:
+        print("Name or password incorrect") #print Name or password incorrect
+else:
+    print("Name incorrect")
+secretnumber = 55
+inputnumber = int(input("Convert to int() after input() or outside input(): "))
+if inputnumber == secretnumber:
+    print("You guessed the secret number")
+    print("A second print statement")
+    secretnumber = 65
+    print("I change the secret number")
+    print("Multiple actions contained in if statement")
+print(secretnumber)
+'''
+Convert to int() after input() or outside input(): 55
+You guessed the secret number
+A second print statement
+I change the secret number
+Multiple actions contained in if statement
+65
+'''
 #yytest.py:30: SyntaxWarning: "is" with a literal. Did you mean "=="? if location is "home" or location is "Vegas":
 # location = "home"
 # if location is "home" or location is "Vegas":
@@ -39,3 +115,139 @@ else:
     print(integernumber, "is not an integer int by checking data type")
 if type(integernumber) == int:
     print("isinstance function better than type(integernumber) == int") #print isinstance function better than type(integernumber) == int
+
+#For loop
+stringisalist = "Zophie"
+for eachstringisalist in stringisalist:
+    print(eachstringisalist, end="_") #print Z_o_p_h_i_e_
+numberslist = [11, 37, 10, 20, 2023, 1]
+for loopeachnumber in numberslist: #Loop or print each number in numberslist list
+    print(loopeachnumber) #print 11\n 37\n 10\n 20\n 2023\n 1
+for loopeachnumber in numberslist[1:4]:
+    print(loopeachnumber) #print 37\n 10\n 20\n
+for forlooprangefunction in range(100, -51, -25):
+    print(forlooprangefunction, end=", ") #print 100, 75, 50, 25, 0, -25, -50,
+#Enumerate function assigns a number with an item in a list.  It returns a tuple of (index position of item,item) in a list.
+enumeratelist = [1, 3, -7, 4, 9, -5, 4]
+print(enumeratelist) #print [1, 3, -7, 4, 9, -5, 4]
+print(enumerate(enumeratelist)) # print <enumerate object at 0x7fed1832a040>
+print(list(enumerate(enumeratelist))) #print [(0, 1), (1, 3), (2, -7), (3, 4), (4, 9), (5, -5), (6, 4)]
+for index, item in enumerate(enumeratelist):
+    print("index position", index, ".  Item is", item, ".")
+    '''
+    index position 0 .  Item is 1 .
+    index position 1 .  Item is 3 .
+    index position 2 .  Item is -7 .
+    index position 3 .  Item is 4 .
+    index position 4 .  Item is 9 .
+    index position 5 .  Item is -5 .
+    index position 6 .  Item is 4 .
+    '''
+#Enumerate list
+primitivebarchart = [19, 3, 15, 7, 11]
+for index, value in enumerate(primitivebarchart):
+    print(index, value, ("*" * value))
+    '''
+    0 19 *******************
+    1 3 ***
+    2 15 ***************
+    3 7 *******
+    4 11 ***********
+    '''
+#List comprehension
+listcomprehensiontriples = [numbertriple * 3 for numbertriple in range(1, 7)]
+print(listcomprehensiontriples) #print [3, 6, 9, 12, 15, 18]
+listcomprehensiontriplesdoubledigits = [numbertriple * 3 for numbertriple in range(1, 7) if numbertriple > 3]
+print(listcomprehensiontriplesdoubledigits) #print [12, 15, 18]
+numbersasstringswithcommas = ", ".join([str(integervariable) for integervariable in range(1, 11)])
+print(numbersasstringswithcommas) #print 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+#Combine lists append lists
+singlea = [1, 2, 3, 4, 5]
+doubleb = [10, 11, 12, 13, 14]
+for eachsinglea, eachdoubleb in zip(singlea, doubleb):
+    print(eachsinglea) #print 1\n 2\n 3\n 4\n 5
+for eachsinglea, eachdoubleb in zip(singlea, doubleb):
+    print(eachdoubleb) #print 10\n 11\n 12\n 13\n 14
+for eachsinglea, eachdoubleb in zip(singlea, doubleb):
+    print(eachdoubleb, eachsinglea) #print 10 1\n 11 2\n 12 3\n 13 4\n 14 5
+firstname = ["Bucky", "Tom", "Taylor"]
+lastname = ["Roberts", "Hanks", "Swift"]
+combinefirstnamelastname = zip(firstname, lastname)
+print(combinefirstnamelastname) #print <zip object at 0x7f4ef275cac0>
+for firstnameforloop, lastnameeachname in combinefirstnamelastname:
+    print(firstnameforloop, end=",")
+    print(lastnameeachname, end=";")
+    print(firstnameforloop, lastnameeachname)
+    '''
+    Bucky,Roberts;Bucky Roberts
+    Tom,Hanks;Tom Hanks
+    Taylor,Swift;Taylor Swift
+    '''
+numbersa = [1, 5, 10, 79]
+numbersb = [2, 3, 42, 79]
+combinenumbersab = zip(numbersa, numbersb)
+for eachnumbersa, eachnumbersb in combinenumbersab:
+    if eachnumbersa > eachnumbersb:
+        print(eachsinglea, "in numbersa is greater than", eachnumbersb)
+    elif eachnumbersb > eachnumbersa:
+        print(eachnumbersb, "in numbersb is greater than", eachsinglea)
+    elif eachnumbersa == eachnumbersb:
+        print("Numbers are equal")
+    else:
+        print("Error")
+    '''
+    2 in numbersb is greater than 5
+    5 in numbersa is greater than 3
+    42 in numbersb is greater than 5
+    Numbers are equal
+    '''
+forloopdictionary = {"Tony": "cool but smells", "Emma": "sits behind me", "Lucy": "asks too many questions"}
+for keyonly in sorted(forloopdictionary.keys()):
+    print(keyonly, end=",") #print Emma,Lucy,Tony,
+for valueonly in forloopdictionary.values():
+    print(valueonly, end=";") #print cool but smells;sits behind me;asks too many questions;
+for key, value in forloopdictionary.items():
+    print(key + " " + value, end="/") #print Tony cool but smells/Emma sits behind me/Lucy asks too many questions/
+#Loop tuples
+tupleunpacking = [(1, 2), (3, 7), (9, 5)]
+for eachtuple in tupleunpacking:
+    print(eachtuple[0], eachtuple[1])
+    '''
+    1 2
+    3 7
+    9 5
+    '''
+for lefttuple, righttuple in tupleunpacking:
+    print(lefttuple * righttuple) #print 2\n 21\n 45
+#Dictionary comprehension and nested dictionaries
+gradesstudents = {"Susan": {"Ma": 45, "En": 37, "Fr": 54}, "Peter": {"Ma": 62, "En": 58, "Fr": 59}, "George": {"Ma": 49, "En": 47, "Fr": 60}, "Mary": {"Ma": 78, "En": 83, "Fr": 62}, "New student Becky": {"Ma": 66, "En": 77, "Fr": 88}}
+print(gradesstudents) #print {'Susan': {'Ma': 45, 'En': 37, 'Fr': 54}, 'Peter': {'Ma': 62, 'En': 58, 'Fr': 59}, 'George': {'Ma': 49, 'En': 47, 'Fr': 60}, 'Mary': {'Ma': 78, 'En': 83, 'Fr': 62}, 'New student Becky': {'Ma': 66, 'En': 77, 'Fr': 88}}
+for key, value, in gradesstudents.items():
+    print(key, value)
+    '''
+    Susan {'Ma': 45, 'En': 37, 'Fr': 54}
+    Peter {'Ma': 62, 'En': 58, 'Fr': 59}
+    George {'Ma': 49, 'En': 47, 'Fr': 60}
+    Mary {'Ma': 78, 'En': 83, 'Fr': 62}
+    New student Becky {'Ma': 66, 'En': 77, 'Fr': 88}
+    '''
+gradesstudentsnames = [names for names in gradesstudents.keys()]
+print(gradesstudentsnames) #print ['Susan', 'Peter', 'George', 'Mary', 'New student Becky']
+for eachgradesstudentsnames in gradesstudentsnames:
+    print(eachgradesstudentsnames, gradesstudents[eachgradesstudentsnames], gradesstudents[eachgradesstudentsnames]["En"])
+    '''
+    Susan {'Ma': 45, 'En': 37, 'Fr': 54} 37
+    Peter {'Ma': 62, 'En': 58, 'Fr': 59} 58
+    George {'Ma': 49, 'En': 47, 'Fr': 60} 47
+    Mary {'Ma': 78, 'En': 83, 'Fr': 62} 83
+    New student Becky {'Ma': 66, 'En': 77, 'Fr': 88} 77
+    '''
+months = {"January": 1, "February": 2, "March": 3, "Add December": 12}
+print(months) #print {'January': 1, 'February': 2, 'March': 3, 'Add December': 12}
+dictionarycomprehension = {monthnumber: monthname for monthname, monthnumber in months.items()}
+print(dictionarycomprehension) #print {1: 'January', 2: 'February', 3: 'March', 12: 'Add December'}
+keylist = ["football", "baseball", "basketball", "hockey"]
+valuelist = ["49ers", "Giants", "Warriors", "Sharks"]
+print(list(zip(keylist, valuelist))) #print [('football', '49ers'), ('baseball', 'Giants'), ('basketball', 'Warriors'), ('hockey', 'Sharks')]
+combinelistsdictionary = {key: value for key, value in list(zip(keylist, valuelist))}
+print(combinelistsdictionary) #print {'football': '49ers', 'baseball': 'Giants', 'basketball': 'Warriors', 'hockey': 'Sharks'}
