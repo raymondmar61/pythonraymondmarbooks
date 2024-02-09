ee9108144f8f816ac6804139041c34d8bc3dfc4a
raymondmar61 <raym61@hotmail.com>
2024-02-09 15:16:06 -0800

    Parent and child classes, inheritance

--- ultimateclasses.py
+++ ultimateclasses.py
@@ -109,34 +109,6 @@ defineobjectThenewbostontuna3.eat() #return Blrrblrlbrlbrbr.  The __init__ is al
 defineobjectThenewbostontuna3.sleep() #return I am sleeping
 defineobjectThenewbostontuna3.swim() #return  I am swimming
 
-class Capitalizeclassesnumbers():
-    def __init__(self):
-        self.number = 0
-    def printnumber(self):
-        print(self.number)
-    def addnumber(self, increase):
-        self.number += increase
-    def subtractnumber(self, decrease):
-        self.number -= decrease
-
-
-instance1 = Capitalizeclassesnumbers()
-instance1.printnumber() #return 0
-instance1.addnumber(5)
-instance1.printnumber() #return 5
-instance1.subtractnumber(50)
-instance1.printnumber() #return -45
-instance2 = Capitalizeclassesnumbers()
-instance2.subtractnumber(100)
-instance2.subtractnumber(250)
-instance2.addnumber(1000)
-instance2.printnumber() #print 650
-instance2 = Capitalizeclassesnumbers()
-instance2.subtractnumber(111)
-instance2.addnumber(999)
-instance2.addnumber(1000)
-instance2.printnumber() #print 1888
-
 class Dog():
     def __init__(self, name, age):
         self.name = name
@@ -173,6 +145,34 @@ secondcallclassdogagain.dogrollover() #return Call dogrollover() inside class Do
 secondcallclassdogagain.dogage() #return Call dogage() inside class Dog().  Lucy age is 3.
 print("Identify the first variable name passing to class Dog() secondcallclassdogagain: {}".format(secondcallclassdogagain.name)) #print Identify the first variable name passing to class Dog() secondcallclassdogagain: Lucy
 print("Identify the second variable age passing to class Dog() secondcallclassdogagain: {}".format(secondcallclassdogagain.age)) #print Identify the second variable age passing to class Dog() secondcallclassdogagain: 3
+
+class Capitalizeclassesnumbers():
+    def __init__(self):
+        self.number = 0
+    def printnumber(self):
+        print(self.number)
+    def addnumber(self, increase):
+        self.number += increase
+    def subtractnumber(self, decrease):
+        self.number -= decrease
+
+
+instance1 = Capitalizeclassesnumbers()
+instance1.printnumber() #return 0
+instance1.addnumber(5)
+instance1.printnumber() #return 5
+instance1.subtractnumber(50)
+instance1.printnumber() #return -45
+instance2 = Capitalizeclassesnumbers()
+instance2.subtractnumber(100)
+instance2.subtractnumber(250)
+instance2.addnumber(1000)
+instance2.printnumber() #print 650
+instance2 = Capitalizeclassesnumbers()
+instance2.subtractnumber(111)
+instance2.addnumber(999)
+instance2.addnumber(1000)
+instance2.printnumber() #print 1888
 
 class Restaurant():
     def __init__(self, restaurantname, cuisinetype):
@@ -215,6 +215,57 @@ lesliediner.addmorecustomers(50)
 lesliediner.printchangenumbercustomers() #return The number of customers changed is 50
 lesliediner.readnumbercustomers() #return 52 customers have been served at Leslie's
 print("Notice use the self variables when referencing the class Restaurant() outside the class. " + lesliediner.differentvariablename + " is the restaurant name. " + lesliediner.cuisinetype + " is the restaurant type.") #print Notice use the self variables when referencing the class Restaurant() outside the class. Leslie's is the restaurant name. American Diner is the restaurant type.
+
+class Human():
+    def __init__(self, initializevariablename, initializevariablegender):
+        self.initializevariablename = initializevariablename
+        self.initializevariablegender = initializevariablegender
+    def returnname(self):
+        return self.initializevariablename
+    def returngender(self):
+        return self.initializevariablegender
+    def printsomething(self, variabletext):
+        print("{} said {}".format(self.initializevariablename, variabletext))
+    def printtwosomethings(self, variabletext1, variabletext2):
+        print("%s said the following: " % (self.initializevariablename))
+        print(variabletext1 + " and " + variabletext2)
+    def givemethreenouns(self, *multiplenouns):
+        print(multiplenouns)
+        print(list(multiplenouns))
+        print("\n".join(multiplenouns))
+
+
+raymondclassobject = Human("nameRaymond", "genderMale")
+print(raymondclassobject) #print <__main__.Human object at 0x7f7061ab24c0>
+print("Print the raymondclassobject initializevariablename value " + raymondclassobject.initializevariablename) #print Print the raymondclassobject initializevariablename value nameRaymond
+print("Return the raymondclassobject initializevariablename " + raymondclassobject.returnname()) #print Return the raymondclassobject initializevariablename nameRaymond
+print("Print the raymondclassobject initializevariablegender value " + raymondclassobject.initializevariablegender) #print Print the raymondclassobject initializevariablegender value genderMale
+print("Return the raymondclassobject initializevariablegender " + raymondclassobject.returngender()) #print Return the raymondclassobject initializevariablegender genderMale
+raymondclassobject.printsomething("send the string to printsomething function") #return nameRaymond said send the string to printsomething function
+raymondclassobject.printtwosomethings("something1", "something2")
+'''
+nameRaymond said the following: 
+something1 and something2
+'''
+raymondclassobject.givemethreenouns("cat", "dog", "bird")
+'''
+('cat', 'dog', 'bird')
+['cat', 'dog', 'bird']
+cat
+dog
+bird
+'''
+spartanclassobject = Human("nameSpartan", "genderFemale")
+print(spartanclassobject) #print <__main__.Human object at 0x7f762d430a00>
+print("Print the spartanclassobject initializevariablename value" + spartanclassobject.initializevariablename) #print Print the spartanclassobject initializevariablename valuenameSpartan
+print("Return the raymondclassobject initializevariablename value again " + raymondclassobject.initializevariablename) #print Return the raymondclassobject initializevariablename value again nameRaymond
+print("Return the spartanclassobject initializevariablegender " + spartanclassobject.returngender()) #print Return the spartanclassobject initializevariablegender genderFemale
+spartanclassobject.printsomething("spartanclassobject sends the string to printsomething function") #return nameSpartan said spartanclassobject sends the string to printsomething function
+spartanclassobject.printtwosomethings("sparta1", "sparta2")
+'''
+nameSpartan said the following: 
+sparta1 and sparta2
+'''
 
 #The class below doesn't use self when calling a function inside the class
 class Boatnoselfinfunction():
@@ -314,3 +365,212 @@ print(defineclassobjectpotter.name) #print HarryPotter
 defineclassobjectpotter.finalhealth()
 print(defineclassobjectpotter.finalhealth()) #print 89
 #print(defineclassobjectpotter.namelength) #print AttributeError: 'Alsocharacter' object has no attribute 'namelength'
+
+#Parent child class
+class Thenewbostonparent():
+    def __init__(self, parentname):
+        self.parentname = parentname
+    def printparentname(self):
+        print("Thenewbostonparent class printparentname method " + self.parentname)
+    def noselfvariableparent(self):
+        print("noselfvariableparent in Thenewbostonparent")
+class Thenewbostonchild(Thenewbostonparent): #class Thenewbostonchild(Thenewbostonparent) inherits def printparentname(self) from class Thenewbostonparent
+    def __init__(self, childname):
+        self.childname = childname
+    def printchildname(self):
+        print("Thenewbostonchild class printchildname method " + self.childname)
+    def noselfvariablechild(self):
+        print("noselfvariablechild in Thenewbostonchild")
+
+
+defineobjectchildthenewbostonchild = Thenewbostonchild("Bucky")
+print(defineobjectchildthenewbostonchild) #print <__main__.Thenewbostonchild object at 0x7f21ed4114c0>
+defineobjectchildthenewbostonchild.printchildname() #return Thenewbostonchild class printchildname method Bucky
+#defineobjectchildthenewbostonchild.printparentname() #return AttributeError: 'Thenewbostonchild' object has no attribute 'parentname'
+defineobjectparentthenewbostonparent = Thenewbostonparent("Roberts")
+print(defineobjectparentthenewbostonparent) #print <__main__.Thenewbostonparent object at 0x7f17532d3eb0>
+defineobjectparentthenewbostonparent.printparentname() #return Thenewbostonparent class printparentname method Roberts
+# defineobjectparentthenewbostonparent.printchildname() #return AttributeError: 'Thenewbostonparent' object has no attribute 'printchildname'
+defineobjectconfirmparentchild = Thenewbostonparent("Any parent")
+defineobjectconfirmparentchild.noselfvariableparent() #return noselfvariableparent in Thenewbostonparent
+#defineobjectconfirmparentchild.noselfvariablechild() #return AttributeError: 'Thenewbostonparent' object has no attribute 'noselfvariablechild'
+defineobjectchildgetparent = Thenewbostonchild("Any child")
+defineobjectchildgetparent.noselfvariablechild() #return noselfvariablechild in Thenewbostonchild
+defineobjectchildgetparent.noselfvariableparent() #return noselfvariableparent in Thenewbostonparent
+
+#Parent child class.  Child class overrides parent class.  No init.
+class Parent():
+    def printlastname(self):
+        print("Parent class lastnamehere")
+class Child():
+    def printfirstname(self):
+        print("Child class firstnamehere")
+    def printlastname(self):
+        print("Child class lastnamehere")
+
+
+parentclassobject = Parent()
+parentclassobject.printlastname() #return Parent class lastnamehere
+childclassobject = Child()
+childclassobject.printfirstname() #return Child class firstnamehere
+childclassobject.printlastname() #return Child class lastnamehere
+
+class Parenttobeinherited():
+    def printlastname(self):
+        print("Parenttobeinherited class lastnamehere")
+    def dontprintinparenttobeinherited(self):
+        print("Parenttobeinherited class.  Print the sentence there.")
+class Child(Parenttobeinherited):
+    def printfirstname(self):
+        print("Child class firstnamehere")
+    def printlastname(self):
+        print("Override Parenttobeinherited() class.  Child class lastnamehere")
+
+
+parentclassobject = Parenttobeinherited()
+parentclassobject.printlastname() #return Parenttobeinherited class lastnamehere
+childclassobject = Child()
+childclassobject.printfirstname() #return Child class firstnamehere
+childclassobject.printlastname() #return Override Parenttobeinherited() class.  Child class lastnamehere.  RM:  It's not an override.  I'm calling printlastname in Child class.
+childclassobject.dontprintinparenttobeinherited() #print Parenttobeinherited class.  Print the sentence there.
+
+#Simple standard multiple inheritance
+class Mario():
+    def move(self):
+        print("Mario is moving")
+class Mushroom():
+    def supersize(self):
+        print("Mario is bigger")
+class Flowerinheritmariomushroomclasses(Mario, Mushroom):
+    def fireball(size):
+        print("Mario fires fireballs")
+
+
+player1 = Mario()
+player1.move() #return Mario is moving
+#player1.supersize() #return AttributeError: 'Mario' object has no attribute 'supersize'
+player2 = Flowerinheritmariomushroomclasses()
+player2.move() #return Mario is moving
+player2.supersize() #return Mario is bigger
+player2.fireball() #return Mario fires fireballs
+
+class Mario():
+    def __init__(self, playername):
+        self.playername = playername
+    def move(self):
+        print(self.playername + " is moving Mario")
+class Eatmushrooom(Mario):
+    def mushroom(self):
+        print(self.playername + " eats the mushroom")
+#class Supermario(Mario, Eatmushrooom): #TypeError: Cannot create a consistent method resolution order (MRO) for bases Mario, Eatmushrooom
+class Supermario(Eatmushrooom):
+    def jumps(self):
+        print(self.playername + " breaks brick blocks")
+
+
+player1mario = Mario("player1")
+player1mario.move() #return player1 is moving Mario
+player1eatmushroom = Eatmushrooom("player1")
+player1eatmushroom.move() #return player1 is moving Mario
+player1eatmushroom.mushroom() #return player1 eats the mushroom
+player1supermario = Supermario("player1")
+player1supermario.move() #return player1 is moving Mario
+player1supermario.mushroom() #return player1 eats the mushroom
+player1supermario.jumps() #return player1 breaks brick blocks
+player2supermarioobjectonly = Supermario("player2")
+print(player2supermarioobjectonly.playername) #print player2
+player2supermarioobjectonly.move() #return player2 is moving Mario
+player2supermarioobjectonly.mushroom() #return player2 eats the mushroom
+player2supermarioobjectonly.jumps() #return player2 breaks brick blocks
+
+class Parentclasscar():
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+    def displaycar(self):
+        thecar = "The car is {} {} {}".format(str(self.year), self.make, self.model)
+        return thecar
+class Childclasscargas(Parentclasscar):
+    def __init__(self, gasbrand):
+        self.gasbrand = gasbrand
+    def driveswithwhatgas(self):
+        thegascar = "The car {} {} {} drives with {}".format(str(self.year), self.make, self.model, self.gasbrand)
+        return thegascar
+class Childclasscargasmakemodelyearinitnoinheritance():
+    def __init__(self, make, model, year, gasbrand):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.gasbrand = gasbrand
+    def driveswithwhatgas(self):
+        thegascar = "The car {} {} {} drives with {}".format(str(self.year), self.make, self.model, self.gasbrand)
+        return thegascar
+class Childclasscargasmakemodelyearinit(Parentclasscar):
+    def __init__(self, make, model, year, gasbrand):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.gasbrand = gasbrand
+    def driveswithwhatgas(self):
+        thegascar = "The car {} {} {} drives with {}".format(str(self.year), self.make, self.model, self.gasbrand)
+        return thegascar
+class Childclasscargasmakemodelyearinitsuper(Parentclasscar):
+    def __init__(self, make, model, year, gasbrand):
+        super().__init__(make, model, year)
+        self.gasbrand = gasbrand
+    def driveswithwhatgas(self):
+        thegascar = "The car {} {} {} drives with {}".format(str(self.year), self.make, self.model, self.gasbrand)
+        return thegascar
+
+
+parentraymondcar = Parentclasscar("Toyota", "Camry", 2005)
+print(parentraymondcar.displaycar()) #print The car is 2005 Toyota Camry
+childraymondcar = Childclasscargas("Chevron")
+print(childraymondcar.driveswithwhatgas) #print <bound method Childclasscargas.driveswithwhatgas of <__main__.Childclasscargas object at 0x7f23f7bd0fd0>>
+#print(childraymondcar.driveswithwhatgas()) #print AttributeError: 'Childclasscargas' object has no attribute 'year'
+#childraymondcarwithallvariables = Childclasscargas("Toyota", "Camry", 2005, "Chevron") #TypeError: __init__() takes 2 positional arguments but 5 were given
+childraymondcarallvariablesnoinheritance = Childclasscargasmakemodelyearinitnoinheritance("Toyota", "Camry", 2500, "Chevron")
+print(childraymondcarallvariablesnoinheritance.driveswithwhatgas()) #print The car 2500 Toyota Camry drives with Chevron
+childraymondcarallvariables = Childclasscargasmakemodelyearinit("Toyota", "Camry", 5002, "Chevron")
+print(childraymondcarallvariables.driveswithwhatgas()) #print The car 5002 Toyota Camry drives with Chevron
+childraymondcarallvariablesinitsuper = Childclasscargasmakemodelyearinitsuper("Toyota", "Camry", 5200, "Chevron")
+print(childraymondcarallvariablesinitsuper.driveswithwhatgas()) #print The car 5200 Toyota Camry drives with Chevron
+print(childraymondcarallvariablesinitsuper.displaycar()) #print The car is 5200 Toyota Camry
+
+class Childclasselectric(Parentclasscar):
+    def __init__(self, make, model, year, batterysize):
+        super().__init__(make, model, year)
+        self.batterysize = batterysize
+    def carbatterysize(self):
+        thebatterycar = "The battery car is {} {} {} driving with {} battery size.".format(str(self.year), self.make, self.model, self.batterysize)
+        print(thebatterycar)
+
+
+childraymondsbatterycar = Childclasselectric("Toyota", "Prius", "2022", 1000)
+print(childraymondsbatterycar.year) #print 2022
+childraymondsbatterycar.carbatterysize() #return The battery car is 2022 Toyota Prius driving with 1000 battery size.
+
+class Functionoutsideclass():
+    def choosemathoperationoutisdeclass(self, tellmemathoperationfunction, *numbers):
+        print("The result is {}".format(tellmemathoperationfunction(*numbers)))
+def mathoperationadd(a, b):
+    return a + b
+def mathoperaetionmultiply(a, b):
+    return a * b
+
+
+defineobjectadd1 = Functionoutsideclass()
+defineobjectadd1.choosemathoperationoutisdeclass(mathoperationadd, 3, 5) #return The result is 8.  It's a method.
+print(defineobjectadd1.choosemathoperationoutisdeclass(mathoperationadd, 3, 5))
+'''
+The result is 8
+None
+'''
+print(mathoperationadd(30, 50)) #print 80
+defineobjectmultiply1 = Functionoutsideclass()
+defineobjectmultiply1.choosemathoperationoutisdeclass(mathoperaetionmultiply, 4, 8) #return The result is 32.  It's a method.
+print(mathoperaetionmultiply(40, 80)) #print 3200
+defineobjectmultiply2 = Functionoutsideclass()
+print(defineobjectmultiply2) #print <__main__.Functionoutsideclass object at 0x7fac5578e880>
+defineobjectmultiply2.choosemathoperationoutisdeclass(mathoperaetionmultiply, 100, 333) #return The result is 33300.  It's a method.
